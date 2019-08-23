1da41904bf9067eff0f3e286e14f1ea6451971f8
Habanossi <hermanni.viljanen@gmail.com>
2019-08-23 14:53:54 +0300

    Changed program to work both between two phones, or between phone and laptop. Connection betwen phone and laptop is off sync. Two phones is preferrable

--- src/org/kde/kdeconnect/Plugins/PingPlugin/PingPlugin.java
+++ src/org/kde/kdeconnect/Plugins/PingPlugin/PingPlugin.java
@@ -28,13 +28,10 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import android.graphics.Color;
 import android.os.Environment;
 import android.util.Log;
 
-import org.apache.commons.net.ntp.NTPUDPClient;
-import org.apache.commons.net.ntp.TimeInfo;
 import org.kde.kdeconnect.Helpers.NotificationHelper;
 import org.kde.kdeconnect.NetworkPacket;
 import org.kde.kdeconnect.Plugins.Plugin;
@@ -44,21 +41,21 @@ import org.kde.kdeconnect_tp.R;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
-import java.io.File;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Date;
 
-import static java.lang.System.exit;
+import java.io.File;
 
 @PluginFactory.LoadablePlugin
 public class PingPlugin extends Plugin {
+    //variables
     private final static String PACKET_TYPE_PING = "kdeconnect.ping";
-    private int i = 1;
-    private String messageLapTop = "";
     private String messagePhone = "";
     private static String TAG = "FingerPrint";
+    private int tick = 1;
+    private String match= "N00%";
+    private String thisFp = "";
+    private int matchcount = 0;
+    private int calc = 0;
+    private boolean first = false;
 
     @Override
     public String getDisplayName() {
@@ -69,129 +66,169 @@ public class PingPlugin extends Plugin {
     public String getDescription() {
         return context.getResources().getString(R.string.pref_plugin_ping_desc);
     }
-    public static final String TIME_SERVER = "pool.ntp.org";
 
-    public static long getCurrentNetworkTime() {
-        NTPUDPClient timeClient = new NTPUDPClient();
-        timeClient.setDefaultTimeout(5000);
-        long string = 0;
-        try{
-            InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
-            try{
-                timeClient.open();
-                TimeInfo timeInfo = timeClient.getTime(inetAddress);
-                long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();   //server time
-                Date time = new Date(returnTime);
-                Log.d(TAG, "Time from " + TIME_SERVER + ": " + time);
-                string = returnTime;
-            }catch(IOException I1){
-                Log.e(TAG, "Can't get response from server.");
-            }
-        }catch(UnknownHostException u1){
-            Log.e(TAG, "Unknown Host");
-        }
 
-        //long returnTime = timeInfo.getReturnTime();   //local device time
-        timeClient.close();
-        return string;
-    }
 
+    /*onPacketReceived handles receiving packets of data from other devices.
+      depending on what stage of the loop the program is, it will either start recording and send the fingerprint back,
+      or compare the fingerprints, send the match info and continue the loop.
+    */
     @Override
     public boolean onPacketReceived(NetworkPacket np) {
-       // long time = getCurrentNetworkTime();
-       // Log.e(TAG, "Time from apache: " + String.valueOf(time));
         if (!np.getType().equals(PACKET_TYPE_PING)) {
             Log.e("PingPlugin", "Ping plugin should not receive packets other than pings!");
             return false;
         }
-
         //Log.e("PingPacketReceiver", "was a ping!");
-
         PendingIntent resultPendingIntent = PendingIntent.getActivity(
                 context,
                 0,
                 new Intent(context, MainActivity.class),
                 PendingIntent.FLAG_UPDATE_CURRENT
         );
-
         int id;
         String message;
         if (np.has("message")) {
             message = np.getString("message");
-            Log.e("HelloPacketSender", message + " is the message");
+            Log.e("PingPacketSender", message + " is the message");
             id = (int) System.currentTimeMillis();
-            messageLapTop = message;
-
-
         } else {
             message = "Ping!";
             id = 42; //A unique id to create only one notification
         }
-        if(i == 1) {
-            String con = message.substring(0,4);
-            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if(message.equals("first")){
 
             Notification noti = new NotificationCompat.Builder(context, NotificationHelper.Channels.DEFAULT)
                     .setContentTitle(device.getName())
-                    .setContentText(con)
+                    .setContentText("first")
                     .setContentIntent(resultPendingIntent)
-                    .setTicker(message)
+                    .setTicker("first")
                     .setSmallIcon(R.drawable.ic_notification)
                     .setAutoCancel(true)
                     .setDefaults(Notification.DEFAULT_ALL)
-                    .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText("first"))
+                    .setLights(Color.WHITE, 3000, 1000)
                     .build();
-
             NotificationHelper.notifyCompat(notificationManager, id, noti);
-            NetworkPacket npp = new NetworkPacket(PACKET_TYPE_PING);
-            npp.set("message", String.valueOf(getCurrentNetworkTime()));
-            device.sendPacket(npp);
-            message = np.getString("message");
-            String wait = message.substring(4);
-            AudioRec.waitTime = Long.parseLong(message.substring(4)) + 3000;
-            Log.e(TAG,"Waittime: " + (AudioRec.waitTime-getCurrentNetworkTime()));
-           // Log.e("mf", "device clock: " + String.valueOf(getCurrentNetworkTime()));
-            //Log.e("mf", "   laptop clock: " + message);
-            /*
-            if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.RECORD_AUDIO},
-                        10);
-                Log.e("audiorecord", "permission for microphone questioned");
-            } else if(ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
-                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        10);
-                Log.e("audiorecord", "permission for writing in external storage questioned");
-            } else {
-                Log.e("AudioRecord","Permission for microphone granted");*/
-
-            AudioRec.Record();
-            // message = AudioRec.getMessage();
-            messagePhone = AudioRec.getMessage();
-            AudioRec.flush();
-            //NotificationHelper.notifyCompat(notificationManager, id, noti);
-            messagePhone += "x";
-            np = new NetworkPacket(PACKET_TYPE_PING);
-            np.set("message", messagePhone);
-            device.sendPacket(np);
-            messagePhone = "";
+            sendFunction();
+            first = true;
         }
-        /*else {
-            int matchCount = 0;
-            for (int i = 0; i < messageLapTop.length() - 1; i++) {
-                //Log.e("hm", messageLapTop.charAt(i) + " vs " + messagePhone.charAt(i));
-                if (messageLapTop.charAt(i) == messagePhone.charAt(i)) {
-                    matchCount++;
+        else if(tick == 1){
+            tick = 0;
+            String wait = message.substring(4);
+            Notification noti = new NotificationCompat.Builder(context, NotificationHelper.Channels.DEFAULT)
+                    .setContentTitle(device.getName())
+                    .setContentText(message.substring(0,4))
+                    .setContentIntent(resultPendingIntent)
+                    .setTicker("Tick 1")
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText("Tick 1"))
+                    .setLights(Color.WHITE, 3000, 1000)
+                    .build();
+            NotificationHelper.notifyCompat(notificationManager, id, noti);
+            AudioRec.waitTime = Long.parseLong(wait);
+            Log.e(TAG,"Waittime: " + (AudioRec.waitTime));
+            AudioRec.Record();
+            messagePhone = AudioRec.getMessage();
+            thisFp = messagePhone;
+            AudioRec.flush();
+            Log.e("hm","All done.");
+            sendFunction();
+        }else if(tick == 0){
+            Notification noti = new NotificationCompat.Builder(context, NotificationHelper.Channels.DEFAULT)
+                    .setContentTitle(device.getName())
+                    .setContentText("Received fingerprint")
+                    .setContentIntent(resultPendingIntent)
+                    .setTicker("Tick 0")
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText("Tick 0"))
+                    .setLights(Color.WHITE, 3000, 1000)
+                    .build();
+            NotificationHelper.notifyCompat(notificationManager, id, noti);
+            while(calc == 1){
+                try{
+                    Thread.sleep(1000);
+                    Log.e(TAG, "waiting for own calculations before receiving fingerprint");
+                }catch(InterruptedException e){
+                    Log.e(TAG, "can't sleep");
                 }
             }
-            Log.e("hm", "Amount of matching points in fingerprints: " + String.valueOf(matchCount));
-        }*/
-        Log.e("hm","All done.");
-
-        //if(i == 1) i = 0;
-       // else i = 1;
-      //  messagePhone = "";
+            Log.e(TAG, "this fingerprint: " + thisFp);
+            Log.e(TAG, "other fingerprint: " + message);
+            for(int i = 0; i < message.length(); i++){
+                if(thisFp.charAt(i) == message.charAt(i)){
+                    matchcount++;
+                }
+            }
+            Log.e(TAG, "matchcount: " + matchcount);
+            if(matchcount > 330){
+                Log.e(TAG, "secure");
+                match = "Y" + (int)((matchcount/540.0)*100) + "%";
+            }else{
+                Log.e(TAG, "not secure");
+                match = "N" + (int)((matchcount/540.0)*100) + "%";
+            }
+            matchcount = 0;
+            tick = 1;
+            Log.e(TAG, "Cycle complete.");
+            sendFunction();
+        }else{
+            Notification noti = new NotificationCompat.Builder(context, NotificationHelper.Channels.DEFAULT)
+                    .setContentTitle(device.getName())
+                    .setContentText("Other notification?")
+                    .setContentIntent(resultPendingIntent)
+                    .setTicker("Other notification?")
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setAutoCancel(true)
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText("Other notification?"))
+                    .setLights(Color.WHITE, 3000, 1000)
+                    .build();
+            NotificationHelper.notifyCompat(notificationManager, id, noti);
+        }
         return true;
+    }
 
+    public void sendFunction(){
+        if(tick == 1){
+            tick = 0;
+            calc = 1;
+            long time = AudioRec.getCurrentNetworkTime();
+            Log.e(TAG, "Current time, sendFunction, tick 1: " + time);
+            /*long time = NetworkTime.time;
+            Log.e(TAG,"getting time");
+            while(time == 0){
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, "nosleep");
+                }
+                time = NetworkTime.time;
+            }*/
+            time += 3000;
+            Log.e(TAG,"sending with tick = 1");
+            NetworkPacket npp = new NetworkPacket(PACKET_TYPE_PING);
+            npp.set("message", match + time);
+            device.sendPacket(npp);
+            AudioRec.waitTime = time;
+            Log.e(TAG,"Waittime: " + (AudioRec.waitTime));
+            AudioRec.Record();
+            thisFp = AudioRec.getMessage();
+            AudioRec.flush();
+            calc = 0;
+        }else if(tick == 0){
+            Log.e(TAG,"sending with tick = 0");
+            NetworkPacket npp = new NetworkPacket(PACKET_TYPE_PING);
+            npp.set("message", messagePhone);
+            device.sendPacket(npp);
+            tick = 1;
+        }
     }
 
     @Override
@@ -202,34 +239,32 @@ public class PingPlugin extends Plugin {
     @Override
     public void startMainActivity(Activity activity) {
         if (device != null) {
+            if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.RECORD_AUDIO},
+                        10);
+                Log.e("audiorecord", "permission for microphone questioned");
+            } else if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        10);
+                Log.e("audiorecord", "permission for writing in external storage questioned");
+            } else {
+                Log.e("AudioRecord", "Permission for microphone granted");
+                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "MediaRecorderSample");
 
-            messagePhone += "x";
-            if(i == 10) {
-                if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.RECORD_AUDIO},
-                            10);
-                    Log.e("audiorecord", "permission for microphone questioned");
-                } else if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            10);
-                    Log.e("audiorecord", "permission for writing in external storage questioned");
-                } else {
-                    Log.e("AudioRecord", "Permission for microphone granted");
-                    File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), "MediaRecorderSample");
-
-                    if (!file.exists())
-                        file.mkdirs();
-                    AudioRec.Record();
-                }
+                if (!file.exists())
+                    file.mkdirs();
             }
-            //messagePhone += AudioRec.getMessage();
-            NetworkPacket np = new NetworkPacket(PACKET_TYPE_PING);
-            np.set("message", messagePhone);
-            device.sendPacket(np);
-            Log.e("HelloPacketSender", "message sent");
-            AudioRec.flush();
-            messagePhone = "";
-            i = 1;
+            //new NetworkTime().execute();
+            if(tick != 1 || first){
+                tick = 1;
+                first = false;
+                Log.e(TAG, "fixing variables");
+            }else{
+                NetworkPacket npp = new NetworkPacket(PACKET_TYPE_PING);
+                npp.set("message", "first");
+                device.sendPacket(npp);
+                first = true;
+            }
         }
     }
 
@@ -252,5 +287,4 @@ public class PingPlugin extends Plugin {
     public String[] getOutgoingPacketTypes() {
         return new String[]{PACKET_TYPE_PING};
     }
-
 }
